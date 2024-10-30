feee5203664297a414f737cec2306068f9bdf86d
cassio202021041 <cassio00710@hotmail.com>
2024-10-29 22:49:14 -0300

    CORRECAO-ENV-01

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,8 +11,8 @@ import { VideoModule } from "./module/video/video.module";
 import { FormsModule } from "@angular/forms";
 import { AdminModule } from "./module/admin/admin.module";
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -38,7 +38,7 @@ import { HomeComponent } from './module/curso/home/home.component';
     LoginComponent,
     HomeLoginComponent,
     HomeComponent,
-    
+
 
 
 
